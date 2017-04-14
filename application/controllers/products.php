3d31b770763d72aeaff4ca29e6553063bffa4a2e
juliandika <juuliandika@gmail.com>
2017-04-14 15:09:00 +0800

    msater

---
+++ application/controllers/products.php
@@ -0,0 +1,10 @@
+<?php
+
+class Products extends CI_Controller {
+  public function index(){
+
+    $this->load->view('welcome_message');
+  }
+}
+
+?>
--- application/views/template/header.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>ciBlog</title>
-  </head>
-  <body>
