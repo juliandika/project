26bb8f89fd66f13ef8eb181ac359624a605087b4
juliandika <juuliandika@gmail.com>
2017-04-14 13:45:54 +0800

    master

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,13 +10,13 @@
     <nav class="navbar navbar-inverse">
       <div class="container">
         <div class="navbar-header">
-          <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+          <a class="navbar-brand" href="<?php echo base_url(); ?>">PrintOnline</a>
         </div>
         <div id="navbar">
           <ul class="nav navbar-nav">
             <li><a href="<?php echo base_url(); ?>">Home</a></li>
-            <li><a href="<?php echo base_url(); ?>about">About</a></li>
-            <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+            <li><a href="<?php echo base_url(); ?>about">Print Dokumen</a></li>
+            <li><a href="<?php echo base_url(); ?>posts">Lihat Harga</a></li>
             <li><a href="<?php echo base_url(); ?>posts">Categories</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
