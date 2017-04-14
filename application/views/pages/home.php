2687170a557505c1f5e5cbe5cb69828500082248
juliandika <juuliandika@gmail.com>
2017-04-14 19:31:45 +0800

    master

---
+++ application/views/pages/home.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p>Selamat Datang di Website Print Dokumen Online</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ciBlog</title>
+    <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+
+    <script src="http://cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="<?php echo base_url(); ?>home">Print Online</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Print Dokumen</a></li>
+            <li><a href="<?php echo base_url(); ?>price">Lihat Harga</a></li>
+            <li><a href="<?php echo base_url(); ?>about">About</a></li>
+
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Create Posts</a></li>
+            <li><a href="#">Create Category</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
