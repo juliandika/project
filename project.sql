1a4158e819f12b84042ebaca6a344d54ed1cf02f
juliandika <juuliandika@gmail.com>
2017-04-16 20:25:15 +0800

    Add files via upload

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2017 at 05:38 PM
+-- Generation Time: Apr 16, 2017 at 02:21 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -29,19 +29,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `products` (
   `id` int(10) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `price` int(10) NOT NULL
+  `description` text NOT NULL,
+  `price` int(10) NOT NULL,
+  `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `price`) VALUES
-(1, 'HVS A4 B/w 70 g', 400),
-(2, 'HVS A4 Color 80 g', 1500),
-(3, 'HVS A3 Color 80 g', 3000),
-(4, 'Art Paper A3 150 g', 4000),
-(5, 'Glossy Photo Paper A4 230 g', 3000);
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`) VALUES
+(1, 'HVS A4 B/w 70 g', 'Deskripsi Produk', 400, 'apolis-coop-leather-sandal-1-537x4021.jpg'),
+(2, 'HVS A4 Color 80 g', 'Deskripsi Produk', 1500, 'aTq67qjnc.jpg'),
+(3, 'HVS A3 Color 80 g', 'Deskripsi Produk', 3000, 'dasi.jpg'),
+(4, 'Art Paper A3 150 g', 'Deskripsi Produk', 4000, 'sepatu-formal-pria-Alphavi-Black-4-400x400.jpg'),
+(5, 'Glossy Photo Paper A4 230 g', 'Deskripsi Produk', 3000, 'apolis-coop-leather-sandal-1-537x4022.jpg');
 
 --
 -- Indexes for dumped tables
