8e13ee5fd0a467879569094c72465a04a742acff
juliandika <juuliandika@gmail.com>
2017-04-24 22:05:21 +0800

    master

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -4,26 +4,33 @@
     <div class="col-sm-12">
       <div id="my-slider" class="carousel slide" data-ride="carousel">
 
+        <ol class="carousel-indicators">
+          <li data-target="#my-slider" data-slide-to="0"></li>
+          <li data-target="#my-slider" data-slide-to="1"></li>
+        </ol>
+
         <div class="carousel-inner" role="listbox">
           <div class="item active">
             <img src="images/1bixbybridge1920x1200.jpg" alt="abc" />
             <div class="carousel-caption">
               <h1>ABC</h1>
-
             </div>
           </div>
           <div class="item">
             <img src="images/1giantfern1920x1200" alt="abc" />
             <div class="carousel-caption">
               <h1>CDE</h1>
-
             </div>
           </div>
         </div>
 
         <a class="left carousel-control" href="#my-slider" role="button" data-slide="prev">
           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#my-slider" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
         </a>
 
       </div>
