3ffab20675d0bc7bdf85ab1664f618cdc0244c22
ernasusanti <ernasusanti91@gmail.com>
2017-04-25 14:40:48 +0800

    tes

--- project.sql
+++ project.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2017 at 08:26 AM
+-- Generation Time: Apr 25, 2017 at 08:39 AM
 -- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.8
+-- PHP Version: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -63,6 +63,35 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`) VALUES
 (3, 'HVS A3 Color 80 g', 'Deskripsi Produk', 3000, 'paper-480538_960_7202.jpg'),
 (4, 'Art Paper A3 150 g', 'Deskripsi Produk', 4000, 'paper-480538_960_7203.jpg'),
 (5, 'Glossy Photo Paper A4 230 g', 'Deskripsi Produk', 3000, 'paper-480538_960_7204.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `nama` varchar(11) NOT NULL,
+  `email` varchar(11) NOT NULL,
+  `notelepon` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`nama`, `email`, `notelepon`, `username`, `password`) VALUES
+('andre', 'andre', 'andre', 'andre', 'andre'),
+('fdh', 'gdch', 'fvcd', 'gcg', 'gbncf'),
+('gfsrgf', 'hfdxhf', 'xfhx', 'fxbfh', 'xgv'),
+('jtgf', 'jfjgc', 'gcnjgcf', 'cngjf', 'fhj'),
+('erna', 'jhfv', 'jmhvj', 'mhvjm', 'vjm'),
+('gfgh', 'ngng', 'hgcngj', 'fjngv', 'hngc'),
+('erna', 'jhjmh', 'jhjmhv', 'jmhvh', 'hv mjhvj'),
+('asdfg', 'ghj', '', '', ''),
+('Made Erna S', 'ernasusanti', '081916544885', 'erna', 'ernaaaa');
 
 --
 -- Indexes for dumped tables
