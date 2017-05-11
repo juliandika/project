28731ea186dcb36b702ef107109c36a700c1a4f3
juliandika <juuliandika@gmail.com>
2017-05-11 11:30:49 +0800

    master

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Online Printing</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>styles.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 
     <script src="http://cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
     <script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
